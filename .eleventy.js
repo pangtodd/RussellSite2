b4d0fb8ff9260216b3f4acd84fd187b35d9a7668
Todd Pangilinan <pang.todd@gmail.com>
2025-03-23 23:59:50 -0700

    adjusted index.njk and config to get loop working.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (eleventyConfig){
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addCollection("books", function(collectionApi){
+        return collectionApi.getFilteredByGlob("books/*.md")
+    });
     return {
         dir:{
             input: ".",
