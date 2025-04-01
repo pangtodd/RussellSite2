8ab79c76588cda74af1c2551017e71af3be2ffa9
Todd Pangilinan <pang.todd@gmail.com>
2025-03-31 23:25:31 -0700

    udpated config.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 module.exports = function (eleventyConfig){
-    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    eleventyConfig.addCollection("blogs", function(collectionApi){
+        return collectionApi.getFilteredByGlob("src/blogs/*.md").sort(function(a,b){
+            return new Date(b.data.date) - new Date(a.data.date);
+        });
+    });
+
     eleventyConfig.addCollection("books", function(collectionApi){
-        return collectionApi.getFilteredByGlob("books/*.md").sort(function(a,b){
+        return collectionApi.getFilteredByGlob("src/books/*.md").sort(function(a,b){
             return a.data.order - b.data.order;
         });
     });
     return {
         dir:{
-            input: ".",
+            input: "src",        
+            includes: "_includes",
+            layouts: "_includes",
             output: "_site"
         }
     };
